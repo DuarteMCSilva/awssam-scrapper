a3405a2cb0aaa0b4d434a3b6601144c12dcefd55
DuarteMCSilva <duartemcsilva@gmail.com>
2024-09-22 15:49:34 +0200

    Added dynamic interval for period selection

--- scrapper-sam/hello_world/app.py
+++ scrapper-sam/hello_world/app.py
@@ -4,11 +4,10 @@ import json
 
 
 def handle_get_prices_request(event, context):
-    request_ticker: str = 'AAPL' #request.GET.get('ticker')
-    period: str = '1y' # request.GET.get('p')
-    interval: str = '1mo'
+    request_ticker: str = event['queryStringParameters']['ticker']
+    period: str = event['queryStringParameters']['p']
 
-    historical_prices = process_historical_prices_by_date(request_ticker, period, interval)
+    historical_prices = process_historical_prices_by_date(request_ticker, period)
 
     close_prices = historical_prices.set_index('Date')['Close'].to_dict()
 
@@ -20,8 +19,8 @@ def handle_get_prices_request(event, context):
         }
     }
 
-def process_historical_prices_by_date(ticker, period, interval):
-    history_df = fetch_historical_prices_from_api(ticker, period, interval)
+def process_historical_prices_by_date(ticker, period):
+    history_df = fetch_historical_prices_from_api(ticker, period)
 
     if history_df.index.name == 'Date':
         history_df = history_df.reset_index()
@@ -29,11 +28,12 @@ def process_historical_prices_by_date(ticker, period, interval):
     history_df['Date'] = history_df['Date'].dt.strftime('%Y%m%d')
     return history_df
 
-def fetch_historical_prices_from_api(ticker: str = None, period_input: str = '', interval: str = '1mo'):
+def fetch_historical_prices_from_api(ticker: str = None, period_input: str = ''):
     if ticker is None:
         return 'No ticker provided!'
 
     period = choose_valid_period(period_input)
+    interval = choose_interval(period)
 
     ticker = yfinance.Ticker(ticker)
     return ticker.history(period=period, interval=interval)
@@ -47,3 +47,17 @@ def choose_valid_period(period_input: str = ''):
         period = period_input
 
     return period
+
+def choose_interval(period):
+    if period == '5y':
+        return '1wk'
+    elif period == '10y':
+        return '1mo'
+    elif period == 'max':
+        return '1wk'
+    elif period == '1d':
+        return '15m'
+    elif period == '5d':
+        return '90m'
+    else:
+        return '1d'
